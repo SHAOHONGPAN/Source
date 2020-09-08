f778290209e648883ff35bfcfcdbdb8771e43dee
SHAOHONGPAN <69775119+SHAOHONGPAN@users.noreply.github.com>
2020-09-08 12:38:13 +1000

    aicontrol

---
+++ Assignment2/enemyAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "enemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ASSIGNMENT2_API AenemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+};
